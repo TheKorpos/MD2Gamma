b272bd9f37808e253841d62a5162c58c3e750e5f
László Dávid Gáti <gatilacika@gmail.com>
2020-10-31 18:27:49 +0100

    Fixed ActivityTransformer

--- hu.bme.mit.md2g/src/main/java/hu/bme/mit/md2g/transformation/ActivityTransformer.java
+++ hu.bme.mit.md2g/src/main/java/hu/bme/mit/md2g/transformation/ActivityTransformer.java
@@ -1,5 +1,6 @@
 package hu.bme.mit.md2g.transformation;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -7,10 +8,10 @@ import java.util.stream.Collectors;
 
 import com.beust.jcommander.internal.Lists;
 import com.nomagic.uml2.ext.magicdraw.actions.mdbasicactions.SendSignalAction;
+import com.nomagic.uml2.ext.magicdraw.activities.mdbasicactivities.ActivityEdge;
 import com.nomagic.uml2.ext.magicdraw.activities.mdbasicactivities.InitialNode;
 import com.nomagic.uml2.ext.magicdraw.activities.mdfundamentalactivities.Activity;
 import com.nomagic.uml2.ext.magicdraw.activities.mdfundamentalactivities.ActivityNode;
-import com.nomagic.uml2.ext.magicdraw.activities.mdintermediateactivities.FinalNode;
 import com.nomagic.uml2.ext.magicdraw.commonbehaviors.mdcommunications.Signal;
 import com.nomagic.uml2.ext.magicdraw.compositestructures.mdports.Port;
 
@@ -23,14 +24,30 @@ public class ActivityTransformer {
 	
 	public static List<Action> tryTransform(Activity activity, Map<Signal, Event> signalTraces, Map<Port, hu.bme.mit.gamma.statechart.interface_.Port> portTraces) {
 		Optional<ActivityNode> init = activity.getNode().stream().filter(InitialNode.class::isInstance).findFirst();
-		Optional<ActivityNode> end = activity.getNode().stream().filter(FinalNode.class::isInstance).findFirst();
-		List<SendSignalAction> sendSignalAction = activity.getNode().stream().filter(SendSignalAction.class::isInstance).map(SendSignalAction.class::cast).collect(Collectors.toList());
-		
 		if (!init.isPresent()) return Lists.newArrayList();
-		if (!end.isPresent()) return Lists.newArrayList();
-		if (sendSignalAction.size() + 2 != activity.getNode().size()) return Lists.newArrayList();
 		
-		return sendSignalAction.stream().map(act -> {
+		List<SendSignalAction> sendActions = new LinkedList<>();
+		
+		if (init.isPresent()) {
+			
+			ActivityNode source = init.get();
+			
+			Optional<SendSignalAction> hopOne = hopOne(source);
+			
+			while (hopOne.isPresent()) {
+				SendSignalAction sendSignalAction = hopOne.get();
+				
+				if (!sendActions.contains(sendSignalAction)) {
+					sendActions.add(sendSignalAction);
+					hopOne = hopOne(sendSignalAction);
+				} else {
+					hopOne = Optional.empty();
+				}
+			}
+			
+		}
+		
+		return sendActions.stream().map(act -> {
 			Signal signal = act.getSignal();
 			Event event = signalTraces.get(signal);
 			Port onPort = act.getOnPort();
@@ -42,5 +59,9 @@ public class ActivityTransformer {
 			 
 			 return raise;
 		}).collect(Collectors.toList());	
+	}
+
+	private static Optional<SendSignalAction> hopOne(ActivityNode source) {
+		return source.getOutgoing().stream().map(ActivityEdge::getTarget).filter(SendSignalAction.class::isInstance).map(SendSignalAction.class::cast).findFirst();
 	}
 }
